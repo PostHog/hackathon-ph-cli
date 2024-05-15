483d768b94b21ec93fb391d69c39f61d7ac42057
Manoel Aranda Neto <marandaneto@gmail.com>
2024-05-15 16:01:10 +0300

    create flag

--- ph/main.py
+++ ph/main.py
@@ -2,7 +2,7 @@ import click
 import logging
 import os
 
-from ph.utils.flags import list_flags
+from ph.utils.flags import create_flag, list_flags
 from .utils.auth import auth, delete_token_from_file
 from rich.logging import RichHandler
 from rich.console import Console
@@ -33,6 +33,14 @@ def flags():
 def list():
     logger.debug("List flags")
     list_flags()    
+
+@flags.command()
+@click.argument('key')
+@click.option('-d', '--description', default="", help='Desc/Name of the flag')
+@click.option('-p', '--rollout-percentage', default=100, help='Rollout percentage of the flag')
+def create(key, description, rollout_percentage):
+    logger.debug("Create flag")
+    create_flag(key, description, rollout_percentage)
 
 def setup_logger():
     # Set the log level for the root logger to NOTSET (this is required to allow handlers to control the logging level)
--- ph/utils/flags.py
+++ ph/utils/flags.py
@@ -1,3 +1,4 @@
+import json
 import inquirer
 import requests
 from ph.utils.auth import get_headers, get_url, read_project_from_file
@@ -14,7 +15,8 @@ def list_flags():
     if response.status_code == 200:
         data = response.json()
         results = data.get('results')
-        display_to_id = {option['name']: option['id'] for option in results}
+
+        display_to_id = {option['key']: option['id'] for option in results}
         choices = list(display_to_id.keys())
 
         questions = [
@@ -45,6 +47,52 @@ def load_flag(id):
     if response.status_code == 200:
         data = response.json()
         logger.info(f"Flag data: {data}")
+    else:
+        if response.status_code == 401:
+            logger.error(f"{response.status_code} Invalid token provided.")
+        elif response.status_code == 302:
+            logger.error(f"Redirection URL: {response.headers.get('Location')}")
+        else:
+            logger.error(f"Error: {response.status_code}")
+
+def create_flag(key, description, rollout_percentage):
+    """Create flag."""
+    headers = get_headers()
+    project_id = read_project_from_file()
+    url = get_url(f'api/projects/{project_id}/feature_flags')
+
+    data = {
+        "key": key,
+        "name": description,
+        "filters": {
+            "groups": [
+            {
+                "properties": [],
+                "rollout_percentage": rollout_percentage,
+                "variant": None
+            }
+            ],
+            "multivariate": None,
+            "payloads": {}
+        },
+        "deleted": False,
+        "active": True,
+        "created_by": None,
+        "is_simple_flag": False,
+        "rollout_percentage": None,
+        "ensure_experience_continuity": False,
+        "experiment_set": None,
+        "features": [],
+        "rollback_conditions": [],
+        "surveys": None,
+        "performed_rollback": False,
+        "can_edit": True,
+        "tags": []
+        }
+
+    response = requests.post(url, headers=headers, json=data, allow_redirects=False)
+    if response.status_code == 201:
+        logger.info(f"Flag created: {key}")
     else:
         if response.status_code == 401:
             logger.error(f"{response.status_code} Invalid token provided.")
